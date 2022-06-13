7c70235ac5f6fc7dca720541f96ec02e8375d2cd
myh <myh@qq.com>
2022-06-13 21:12:14 +0800

    first commit

---
+++ src/main/java/com/myh/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.myh.git;
+
+/**
+ * @author myh
+ * @ClassName GitTest
+ * @Description
+ * @date 2022/06/13 21:08
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
